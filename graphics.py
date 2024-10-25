48d23beea43a092061ff4755cb8694989dfe0dde
JessFint <45701595+jFinton@users.noreply.github.com>
2024-10-25 11:46:21 -0400

    created point and line classes for simple vector drawing

--- graphics.py
+++ graphics.py
@@ -21,3 +21,21 @@ class Window():
         
     def close(self):
         self.is_running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+    
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line():
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas: Canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
+
+    line1 = Line(Point(20, 40), Point(200,40))
+    line2 = Line(Point(50, 200), Point(100, 400))
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "blue")
+
     win.wait_for_close()
 
 
